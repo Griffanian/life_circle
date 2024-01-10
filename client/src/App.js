8febae609bc65b7250fcbeb25af334660e5e944f
Griffanian <milesbloom526@gmail.com>
2024-01-10 13:24:43 +0000

    added another log

--- client/src/App.js
+++ client/src/App.js
@@ -46,7 +46,7 @@ function App() {
   const [userName, setUserName] = useState('');
 
   useEffect(() => {
-    console.log(process.env)
+    console.log(process.env, process.env.REACT_APP_BACKEND_URL)
     getIsLoggedIn()
       .then((res) => {
         if (res && res.loggedIn && res.username) {
